73d9e8041ff32bbfe759c9f88ca0ba1e3593b3d4
Marylin2000 <severside2000@gmail.com>
2024-10-22 23:06:24 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Courses from "./pages/Courses";
 import Blogs from "./pages/Blogs";
 import Contacts from "./pages/Contacts";
 import About from "./pages/About";
+
 import Team from "./pages/Team";
 import CheatSheetPage from "./pages/CheatSheets";
 
@@ -16,20 +17,20 @@ const App = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-        {/* Fixed Header */}
-        <div className="fixed w-full z-20">
+    
           <Header />
-        </div>
-        {/* Main content adjusted with margin-top */}
-        <main className="flex-1 mt-[8vh]">
+        
+        <main className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/about" element={<About />} />
+
             <Route path="/signup" element={<Signup />} />
             <Route path="/courses" element={<Courses />} />
             <Route path="/blogs" element={<Blogs />} />
             <Route path="/cheatsheets" element={<CheatSheetPage />} />
+
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/team" element={<Team />} />
           </Routes>
